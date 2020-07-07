bba6d82691a126cdb09687f37c5562a93717f27b
kaidornel <kai_dornel@hotmail.com>
2020-07-07 13:09:29 -0400

    added backup api data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :corona_data
   resources :conditions
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
